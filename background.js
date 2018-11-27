4c6929f084b6c32625bf08b7d41642521e28cd91
burdukowsky <burdukowskystas@gmail.com>
2018-11-27 13:35:35 +0700

    show badge text after click - if OK then 'ok' else 'er'

--- background.js
+++ background.js
@@ -1,13 +1,29 @@
 "use strict";
 
+browser.browserAction.setBadgeTextColor({color: "white"});
+
+let badgeTimeout;
+
+function showBadge(text, bgColor) {
+    browser.browserAction.setBadgeText({text: text});
+    browser.browserAction.setBadgeBackgroundColor({color: bgColor});
+    clearTimeout(badgeTimeout);
+    badgeTimeout = setTimeout(() => {
+        browser.browserAction.setBadgeText({text: ""});
+    }, 3000);
+}
+
 function onError(error) {
     console.error(`Error: ${error}`);
+    showBadge("er", "#dc3545");
 }
 
 function sendMessageToTabs(tabs) {
     for (let tab of tabs) {
         browser.tabs.sendMessage(tab.id, {action: "getLocation"}).then(response => {
-            navigator.clipboard.writeText(response).then(null, onError);
+            navigator.clipboard.writeText(response).then(() => {
+                showBadge("ok", "#28a745");
+            }, onError);
         }).catch(onError);
     }
 }
